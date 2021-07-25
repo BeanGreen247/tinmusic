94659743b2ce945b45e8ff45d052797a795be853
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2021-07-25 08:30:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 make
 sudo cp -r tinmusic /usr/local/bin/tinmusic
+sudo mkdir -p /usr/local/man/man1/
 install -g 0 -o 0 -m 0644 tinmusic.1 /usr/local/man/man1/
 sudo gzip -f /usr/local/man/man1/tinmusic.1
 sudo mandb
